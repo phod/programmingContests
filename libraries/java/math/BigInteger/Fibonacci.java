b6f42384c956047f33d5f80c2c840007a3aa86aa
phod <philip.hodder@gmail.com>
2014-09-04 20:57:30 +1200

    Added licence to Fibonnaci.java

--- libraries/java/math/BigInteger/Fibonacci.java
+++ libraries/java/math/BigInteger/Fibonacci.java
@@ -1,3 +1,20 @@
+/*
+    Calculates the nth fibonacci number, using the BigInteger class
+    Copyright (C) 2014  phod
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import java.math.BigInteger;
 
 public class Fibonacci {
